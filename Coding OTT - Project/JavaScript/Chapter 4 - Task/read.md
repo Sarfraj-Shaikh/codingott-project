231905357f7b2d1daef28ac3a8f7e3f3f841e2c2
Mo. Sarfraj Shaikh <137554535+Sarfraj-Shaikh@users.noreply.github.com>
2026-01-01 23:42:31 +0530

    Add files via upload

---
+++ Coding OTT - Project/JavaScript/Chapter 4 - Task/read.md
@@ -0,0 +1,11 @@
+## Chapter 4: Predefined (Built-in) Function Questions
+
+Write a program that calls the alert() function using a mouse event.
+
+Write a program that uses the open() function to open a video.
+
+Write a program to close the browser window.
+
+Write a program that prints the * symbol using the write() function.
+
+Write a program that calls the print() function.
---
+++ Coding OTT - Project/JavaScript/Chapter 4 - Task/script.js
@@ -0,0 +1,24 @@
+//  1. Write a program that calls the alert() function using a mouse event.
+function alertFun() {
+    window.alert("Button Clicked");
+}
+
+// 2. Write a program that uses the open() function to open a video.
+function vidFun() {
+    window.open("https://www.youtube.com");
+}
+
+// 3. Write a program to close the browser window.
+function closeWin() {
+    window.close();
+}
+
+// 4. Write a program that prints the * symbol using the write() function.
+function writeStar() {
+    document.write("<h1> * </h1>");
+}
+
+// 5. Write a program that calls the print() function.
+function printPage() {
+    window.print();
+}

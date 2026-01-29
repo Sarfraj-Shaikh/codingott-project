93270444d9bb86b9afed2f4c18c1b97db441ac65
Mo. Sarfraj Shaikh <137554535+Sarfraj-Shaikh@users.noreply.github.com>
2026-01-29 15:11:46 +0530

    Add files via upload

---
+++ Coding OTT - Project/JavaScript/Chapter 24 - Part  5/README.md
@@ -0,0 +1,16 @@
+## Chapter 24: Part 5
+
+### Task:
+
+- Isme check karo ki user ne **mobile number** input field me fill kiya hai ya nahi.  
+- Ye bhi verify karo ki **password** enter kiya gaya hai ya nahi.  
+- Ensure karo ki **Confirm Password**, password se match kar raha hai ya nahi.  
+
+### Expected Behavior:
+
+- Jab saare required input fields properly filled ho  
+- Aur **password** aur **confirm password** match kare  
+
+👉 Tab **Submit** button enable ho jana chahiye.  
+👉 Button ka **cursor icon** change ho jana chahiye.  
+👉 Button ka **background color** bhi update ho jana chahiye.
---
+++ Coding OTT - Project/JavaScript/Chapter 24 - Part  5/script.js
@@ -0,0 +1,101 @@
+// ## Task
+
+/* ========== First Name Validation Start Here! ========== */
+function firstNameVal() {
+    let fName = document.getElementById("firstName").value;
+    let fInput = document.getElementById("firstName");
+    let errorMsg = document.getElementById("error-text");
+    let tick = document.getElementById("tick");
+    let capital = /[A-Z]/g;
+
+    errorMsg.style.display = "block";
+
+    (fName === "") ? errorMsg.innerHTML = "First Name is required" : fName.charAt(0).match(capital) ? errorMsg.style.display = "none" : errorMsg.innerHTML = "First letter is must be capital";
+    (fName === "") ? fInput.style.border = "1px solid red" : fName.charAt(0).match(capital) ? fInput.style.border = "" : fInput.style.border = "1px solid red";
+    (fName === "") ? tick.style.display = "none" : fName.charAt(0).match(capital) ? tick.style.display = "block" : tick.style.display = "none";
+    (fName === "") ? fInput.setAttribute("class", "animate__animated animate__headShake") : fName.charAt(0).match(capital) ? tick.style.display = "block" : fInput.setAttribute("class", "animate__animated animate__headShake");
+}
+/* ========== First Name Validation End Here! ========== */
+
+/* ========== Last Name Validation Start Here! ========== */
+function lastNameVal() {
+    let lName = document.getElementById("lastName").value;
+    let lInput = document.getElementById("lastName");
+    let errorMsg = document.getElementById("error-text2");
+    let tick = document.getElementById("tick2");
+    let capital = /[A-Z]/g;
+
+    errorMsg.style.display = "block";
+
+    (lName === "") ? errorMsg.innerHTML = "Last Name is required" : lName.charAt(0).match(capital) ? errorMsg.style.display = "none" : errorMsg.innerHTML = "First letter is must be capital";
+
+    (lName === "") ? lInput.style.border = "1px solid red" : lName.charAt(0).match(capital) ? lInput.style.border = "" : lInput.style.border = "1px solid red";
+
+    (lName === "") ? tick.style.display = "none" : lName.charAt(0).match(capital) ? tick.style.display = "block" : tick.style.display = "none";
+
+    (lName === "") ? lInput.setAttribute("class", "animate__animated animate__headShake") : lName.charAt(0).match(capital) ? tick.style.display = "block" : lInput.setAttribute("class", "animate__animated animate__headShake");
+}
+/* ========== Last Name Validation End Here! ========== */
+
+/* ========== Mobile Number Validation Start Here! ========== */
+function mobileNumVal() {
+    let mNum = document.getElementById("mobileNumber").value;
+    let mInput = document.getElementById("mobileNumber");
+    let errorMsg = document.getElementById("error-text4");
+    let tick = document.getElementById("tick4");
+    let indNum = /[7-9]/g;
+
+    errorMsg.style.display = "block";
+
+    (mNum === "") ? errorMsg.innerHTML = "Mobile Number is required" : mNum.charAt(0).match(indNum) ? errorMsg.style.display = "none" : errorMsg.innerHTML = "Only Indian Number is accepted";
+
+    (mNum === "") ? mInput.style.border = "1px solid red" : mNum.charAt(0).match(indNum) ? mNum.length === 10 ? mInput.style.border = "" : mInput.style.border = "1px solid red" : errorMsg.innerHTML = "Mobile Number must be 10 Digits";
+
+    (mNum === "") ? tick.style.display = "none" : mNum.length === 10 ? mNum.charAt(0).match(indNum) ? tick.style.display = "block" : errorMsg.innerHTML = "Only Indian Number is accepted" : tick.style.display = "none";
+
+    (mNum === "") ? mInput.setAttribute("class", "animate__animated animate__headShake") : mName.charAt(0).match(capital) ? tick.style.display = "block" : mInput.setAttribute("class", "animate__animated animate__headShake");
+}
+/* ========== Mobile Number Validation End Here! ========== */
+
+/* ========== Username Validation Start Here! ========== */
+function uNameVal() {
+    let fName = document.getElementById("firstName").value;
+    let lName = document.getElementById("lastName").value;
+    let uName = document.getElementById("userName");
+    let errorMsg = document.getElementById("error-text5");
+    let tick = document.getElementById("tick5");
+
+    errorMsg.style.display = "block";
+
+    (fName === "" || lName === "") ? (errorMsg.innerHTML = "Both first and last name are required", uName.value = "", uName.style.border = "1px solid red", tick.style.display = "none") : (errorMsg.style.display = "none", uName.value = (fName + lName).toLowerCase(), uName.style.border = "", tick.style.display = "block", uName.setAttribute("disabled", "disabled"));
+}
+/* ========== Username Validation End Here! ========== */
+
+/* ========== Password Validation Start Here! ========== */
+function passVal() {
+    let pwd = document.getElementById("password").value;
+    let pwdInput = document.getElementById("password");
+    let errorMsg = document.getElementById("error-text6");
+    let tick = document.getElementById("tick6");
+
+    errorMsg.style.display = "block";
+
+    (pwd === "") ? (errorMsg.innerHTML = "Password is required", tick.style.display = "none", pwdInput.style.border = "1px solid red" ) : (tick.style.display = "block", errorMsg.style.display = "none", pwdInput.style.border = "");
+}
+/* ========== Password Validation End Here! ========== */
+
+/* ========== Confirm Password Validation Start Here! ========== */
+function conPassVal() {
+    let pwd = document.getElementById("password").value;
+    let conPwd = document.getElementById("conPassword").value;
+    let pwdInput = document.getElementById("password");
+    let conPwdInput = document.getElementById("conPassword");
+    let errorMsg = document.getElementById("error-text7");
+    let tick = document.getElementById("tick7");
+    let btn = document.getElementById("subBtn");
+
+    errorMsg.style.display = "block";
+
+    (conPwd === "") ? (errorMsg.innerHTML = "Confirm Password is required", conPwdInput.style.border = "1px solid red") : (pwd === conPwd) ? (btn.disabled = false, btn.style.cursor = "pointer", btn.style.backgroundColor = "blue", tick.style.display = "block", errorMsg.style.display = "none", conPwdInput.style.border = "") : (btn.disabled = true, btn.style.cursor = "not-allowed", btn.style.backgroundColor = "#007bff", errorMsg.innerHTML = "Confirm password is not match", tick.style.display = "none", conPwdInput.style.border = "1px solid red");
+}
+/* ========== Confirm Password Validation End Here! ========== */

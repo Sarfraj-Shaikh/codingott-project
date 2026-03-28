51fe34d362732c6deab6322eac43a6741ff51f57
Mo. Sarfraj Shaikh <137554535+Sarfraj-Shaikh@users.noreply.github.com>
2026-03-28 17:39:43 +0530

    Add files via upload

---
+++ Coding OTT - Project/JavaScript/Chapter 27 - Function Parameter/README.md
@@ -0,0 +1,14 @@
+## Chapter 27: Function Parameter
+
+### Task:
+Ek simple project banaiye jisme ek *Function Call* ho.  
+
+- Jab user *color icon* par click kare, to *background ka color change* ho.  
+- Us card/sidebar ke andar *multiple color options* hon.  
+- Jab kisi *color option* par click kiya jaye, to:
+  - *Background color* change ho  
+  - *h1 text ka color* bhi change ho  
+
+Please ensure that:
+- Functionality smooth ho  
+- Code properly structured aur readable ho
---
+++ Coding OTT - Project/JavaScript/Chapter 27 - Function Parameter/script.js
@@ -0,0 +1,56 @@
+// ### 1. Color Change
+
+function changeColorRed() {
+
+    let bg = document.getElementById("contain");
+    let txt = document.getElementById("heading");
+
+    bg.style.backgroundColor = "red";
+    txt.style.color = "#FFFFFF";
+}
+
+function changeColorBlue() {
+
+    let bg = document.getElementById("contain");
+    let txt = document.getElementById("heading");
+
+    bg.style.backgroundColor = "blue";
+    txt.style.color = "#FFFFFF";
+}
+
+function changeColorGreen() {
+
+    let bg = document.getElementById("contain");
+    let txt = document.getElementById("heading");
+
+    bg.style.backgroundColor = "green";
+    txt.style.color = "#FFFFFF";
+}
+
+function changeColorYellow() {
+
+    let bg = document.getElementById("contain");
+    let txt = document.getElementById("heading");
+
+    bg.style.backgroundColor = "yellow";
+    txt.style.color = "#000000";
+}
+
+function changeColorDark() {
+
+    let bg = document.getElementById("contain");
+    let txt = document.getElementById("heading");
+
+    bg.style.backgroundColor = "black";
+    txt.style.color = "#FFFFFF";
+}
+
+function changeColorWhite() {
+
+    let bg = document.getElementById("contain");
+    let txt = document.getElementById("heading");
+
+    bg.style.backgroundColor = "#FFFFFF";
+    txt.style.color = "#000000";
+}
+
